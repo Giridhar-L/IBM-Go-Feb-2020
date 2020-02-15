1fedbf40a6e1e25f0099fea9df213ca3790fd77d
Magesh Kuppan <tkmagesh77@gmail.com>
2020-02-15 14:01:51 +0530

    Parsing JSON
    This reverts commit 75684af906b0cfadacdda125c3da65a87f17ef24.

--- 03-sorting.go
+++ 03-sorting.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 import "sort"
 
-func main() {
+func main3() {
 
-    strs := []string{"c", "a", "b"}
-    sort.Strings(strs)
-    fmt.Println("Strings:", strs)
-    
-    ints := []int{7, 2, 4}
-    sort.Ints(ints)
-    fmt.Println("Ints:   ", ints)
-    
-    s := sort.IntsAreSorted(ints)
-    fmt.Println("Sorted: ", s)
+	strs := []string{"c", "a", "b"}
+	sort.Strings(strs)
+	fmt.Println("Strings:", strs)
+
+	ints := []int{7, 2, 4}
+	sort.Ints(ints)
+	fmt.Println("Ints:   ", ints)
+
+	s := sort.IntsAreSorted(ints)
+	fmt.Println("Sorted: ", s)
 }
